0366f3ca23310db344b40a5fb3b0967c7c493b8c
harrsh206 <harshinkriss88@gmail.com>
2024-10-16 14:52:23 +0400

    coding is cool
    coding is cool

---
+++ exercise 01/codingiscool.py
@@ -0,0 +1,10 @@
+#assigning the one word to the variable word1
+word1 = "coding"
+
+#assigning the one word to the variable word2
+word2 = "is"
+
+#assigning the one word to the varibale word3
+word3 = "cool"
+c = word1 + " " + word2 + " " + word3 
+print(c)
