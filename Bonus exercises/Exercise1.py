93b21b049f97808e7a12c47deea110bc4d4e78f1
harrsh206 <harshinkriss88@gmail.com>
2024-11-20 19:06:00 +0400

    Exercise1.py
    Bonus exercise

---
+++ Bonus exercises/Exercise1.py
@@ -0,0 +1,13 @@
+#Strings Concatenation: 
+#Write three strings in different variables and print the output as one string. 
+
+#assigning the one word to the variable word1
+wordalpha = "Welcome"
+
+#assigning the one word to the variable word2
+wordbeta = "to"
+
+#assigning the one word to the varibale word3
+wordcharlie = "over world"
+c = wordalpha + " " + wordbeta + " " + wordcharlie 
+print(c)
