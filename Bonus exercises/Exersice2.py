599bdcff5aa7e6e47a270ed978beb0e525d5c8d8
harrsh206 <harshinkriss88@gmail.com>
2024-11-20 19:06:34 +0400

    Exercise2.py
    Bonus Exercise

---
+++ Bonus exercises/Exersice2.py
@@ -0,0 +1,11 @@
+#Variables: 
+#Assign a message to a variable, and print that message. Then change the value of the 
+#variable to a new message, and print the new message.
+
+#Assigning a variable called text and value.
+text1 = "hello, everybody!"
+print(text1) #to display the first value
+
+#Assigning  different value to the same variable.
+text1 = "goodbye, everybody!"
+print(text1) #To display the new value
