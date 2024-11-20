5be3eff353b008b49c2f652dd5fd3ab8a3e4577d
harrsh206 <harshinkriss88@gmail.com>
2024-11-20 19:09:45 +0400

    Exercise6.py
    Bonus exercise

---
+++ Bonus exercises/Exersise6.py
@@ -0,0 +1,19 @@
+#Dictionaries: 
+#Use a dictionary to store information about a person you know.Store their first name, 
+#last name, age, and the city in which they live.  
+#You should have keys such as first_name, last_name, age, and city. Print each piece of 
+#information stored in your dictionary.
+
+#Creating the dictionary
+info_of_person = {
+    "First name": "harshin",
+    "Last name": "kriss",
+    "Age": 19,
+    "City" : "Sharjah"
+}
+
+#Printing each piece of information
+print(f"First Name: {info_of_person['First name']}")
+print(f"Last name: {info_of_person['Last name']}")
+print(f"Age: {info_of_person['Age']}")
+print(f"City: {info_of_person['City']}")
