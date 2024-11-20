3da6700e7e440c158048081f569bc3c9431e414f
harrsh206 <harshinkriss88@gmail.com>
2024-11-20 19:07:43 +0400

    Exercise4.py
    Bonus exercise

---
+++ Bonus exercises/Exercise4.py
@@ -0,0 +1,12 @@
+#List -Names  
+#Store the names of a few of your friends in a list called names. Print each person’s name 
+#by accessing each element in the list, one at a time.
+
+#Store classmate names in the list
+classmate = ["Ace","Bonny","Charles","Danny"]
+
+#Prints each name at a time
+print(classmate[0])
+print(classmate[1])
+print(classmate[2])
+print(classmate[3])
