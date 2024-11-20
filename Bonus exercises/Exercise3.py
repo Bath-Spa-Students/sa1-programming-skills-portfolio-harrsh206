8b23d5181f1eb16d062cb17d6730567ffa29a296
harrsh206 <harshinkriss88@gmail.com>
2024-11-20 19:06:56 +0400

    Exercise3.py
    Bonus exercise

---
+++ Bonus exercises/Exercise3.py
@@ -0,0 +1,12 @@
+#Variables (Favorite Number): 
+#Use a variable to represent your favorite number. Then, using that variable, create a 
+#message that reveals your favorite number. Print that message.
+
+#To show your favorite number
+favorite_number = 9
+
+#To reaveal your favorite number
+text = f"My favorite number is {favorite_number}."
+
+#To display the number
+print(text)
